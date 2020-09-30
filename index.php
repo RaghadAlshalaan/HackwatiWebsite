36000b2a87fde353375b81c659a5b8de9be43c93
Raghad <ra1433ra@gmail.com>
2020-09-30 21:04:08 +0300

    members

--- index.php
+++ index.php
@@ -277,10 +277,48 @@
         <section class="blog-one blog-one__home thm-gray-bg" id="news">
             <div class="container">
                 <div class="block-title text-center">
-                    <h2 class="block-title__title">Checkout Our <span>Latest</span> <br> News & Articles.</h2><!-- /.block-title__title -->
+                    <h2 class="block-title__title">المطورين</h2><!-- /.block-title__title -->
                 </div><!-- /.block-title -->
                 <div class="row">
                     <div class="col-lg-4 col-md-12 col-sm-12 wow fadeInUp" data-wow-duration="1500ms">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <img src="assets/images/blog/blog-1-1.jpg" alt="">
+                                <a class="blog-one__more-link" href="news-details.html"><i class="fa fa-link"></i>
+                                    <!-- /.fa fa-link --></a>
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <ul class="list-unstyled blog-one__meta">
+                                    <li><a href="#">22 Oct, 2019</a></li>
+                                    <li><a href="#">2 Comments</a></li>
+                                </ul><!-- /.list-unstyled -->
+                                <h3 class="blog-one__title">
+                                    <a href="news-details.html">Pre and post launch mobile app marketing pitfalls to avoid</a>
+                                </h3><!-- /.blog-one__title -->
+                                <a href="news-details.html" class="blog-one__link">Read More</a><!-- /.blog-one__link -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                                        <div class="col-lg-4 col-md-12 col-sm-12 wow fadeInUp" data-wow-duration="1500ms">
+                        <div class="blog-one__single">
+                            <div class="blog-one__image">
+                                <img src="assets/images/blog/blog-1-1.jpg" alt="">
+                                <a class="blog-one__more-link" href="news-details.html"><i class="fa fa-link"></i>
+                                    <!-- /.fa fa-link --></a>
+                            </div><!-- /.blog-one__image -->
+                            <div class="blog-one__content">
+                                <ul class="list-unstyled blog-one__meta">
+                                    <li><a href="#">22 Oct, 2019</a></li>
+                                    <li><a href="#">2 Comments</a></li>
+                                </ul><!-- /.list-unstyled -->
+                                <h3 class="blog-one__title">
+                                    <a href="news-details.html">Pre and post launch mobile app marketing pitfalls to avoid</a>
+                                </h3><!-- /.blog-one__title -->
+                                <a href="news-details.html" class="blog-one__link">Read More</a><!-- /.blog-one__link -->
+                            </div><!-- /.blog-one__content -->
+                        </div><!-- /.blog-one__single -->
+                    </div><!-- /.col-lg-4 -->
+                                        <div class="col-lg-4 col-md-12 col-sm-12 wow fadeInUp" data-wow-duration="1500ms">
                         <div class="blog-one__single">
                             <div class="blog-one__image">
                                 <img src="assets/images/blog/blog-1-1.jpg" alt="">
