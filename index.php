5ae41695c80b59480eaef2560cbce9824de783a9
Raghad <ra1433ra@gmail.com>
2020-09-30 21:26:13 +0300

    justify-content-md-center

--- index.php
+++ index.php
@@ -279,7 +279,7 @@
                 <div class="block-title text-center">
                     <h2 class="block-title__title">المطورين</h2><!-- /.block-title__title -->
                 </div><!-- /.block-title -->
-                <div class="row">
+                <div class="row justify-content-md-center">
                     <div class="col-lg-2 col-md-12 col-sm-12 wow fadeInUp" data-wow-duration="1500ms">
                         <div class="blog-one__single">
                             <div class="blog-one__image">
