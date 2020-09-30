993dbe1e03e70bbcb927f83cfc1deed444bf8af9
Raghad <ra1433ra@gmail.com>
2020-09-30 10:50:34 +0300

    form

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ar">
+<html lang="AR">
 
 <head>
     <meta charset="UTF-8">
@@ -646,11 +646,7 @@
                 <div class="block-title text-center">
                     <h2 class="block-title__title">Subscribe Our <span>Newsletter</span> <br> to Get New Updates.</h2><!-- /.block-title__title -->
                 </div><!-- /.block-title -->
-                <form action="#" class="mailchimp-one__mc-form mc-form" data-url="https://xyz.us18.list-manage.com/subscribe/post?u=20e91746ef818cd941998c598&amp;id=cc0ee8140e">
-                    <input type="email" id="mc-email" class="formInput" placeholder="Email address">
-                    <button type="submit"><i class="dimon-icon-right-arrow"></i>
-                        <!-- /.fa fa-paper-plane --></button>
-                </form><!-- /.mailchimp-one__mc-form -->
+    
                 <div class="mc-form__response"></div><!-- /.mc-form__response -->
             </div><!-- /.container -->
         </section><!-- /.mailchimp-one -->
