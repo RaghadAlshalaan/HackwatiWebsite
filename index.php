0ead127e24a1e6aca88d59bea7b8500d696e473a
ghedaa <ghedaa.aj@gmail.com>
2020-10-03 16:11:17 +0300

    hh

--- index.php
+++ index.php
@@ -204,17 +204,17 @@
                             <div class="block-title text-left">
                                 <h2 class="block-title__title">Interact with <span>Customers</span> <br> on Single Platform.</h2><!-- /.block-title__title -->
                             </div><!-- /.block-title -->
-                            <div class="cta-two__text">
+                            <!-- <div class="cta-two__text">
                                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem Ipsum is simply dummy text of the printing laborum perspiciatis unde.</p>
                                 <p>Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit lorem ipsum anim id est.</p>
-                            </div><!-- /.cta-two__text -->
+                            </div><!-- /.cta-two__text --> -->
                             <a href="#" class="thm-btn"><span>Get Started</span></a>
                         </div><!-- /.cta-two__content -->
                     </div><!-- /.col-lg-6 -->
                 </div><!-- /.row -->
             </div><!-- /.container -->
         </section><!-- /.cta-two --> -->
-        
+
         <section class="app-shot-one" id="screenshots">
             <img src="assets/images/background/app-shot-bg.png" alt="Awesome Image" class="app-shot-one__bg" />
             <div class="container-fluid">
